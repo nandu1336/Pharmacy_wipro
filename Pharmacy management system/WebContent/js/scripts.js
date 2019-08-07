ed9b8677d994672bce770f0477bdb891e0248027
nandakishore <nk732100@gmail.com>
2019-08-07 18:01:40 +0530

    Add files via upload

--- Pharmacy management system/WebContent/js/scripts.js
+++ Pharmacy management system/WebContent/js/scripts.js
@@ -1,7 +1,3 @@
-/**
- * 
- */
-
 function show_password(){
 	var field = document.getElementById("password");
 	if(field.type == "password"){
@@ -9,5 +5,31 @@ function show_password(){
 	}
 	else if(field.type == "text"){
 		field.type = "password";
+	}
+}
+
+function validate(){
+	var id = document.getElementById("id");
+	var title = document.getElementById("title");
+	var quantity = document.getElementById("quantity");
+	var price = document.getElementById("price");
+	if(id.value.length == 0){
+		alert("Medicine Id can not be empty");
+		this.load();
+	}
+	else if(title.value.length == 0){
+		alert("Title can not be empty");
+		this.load();
+	}
+	else if(quantity.value.length == 0){
+		alert("quantity can not be empty");
+		this.load();
+	}
+	else if(price.value.length == 0){
+		alert("price can not be empty");
+		this.load();
+	}
+	else{
+		document.getElementById("form").submit();
 	}
 }
